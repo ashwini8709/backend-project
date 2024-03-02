1622996f56dfe07042858386f6bd0fe0f99fca87
Ashwini Srivastava <ashwinisrivastava8541@gmail.com>
2024-03-03 03:02:08 +0530

    multer for fs

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,9 @@
 import mongoose, { Schema } from "mongoose";
 
+import { Jwt } from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+
 const userSchema= new Schema(
     {
         username:{
@@ -52,5 +56,64 @@ const userSchema= new Schema(
     }
     
 )
+
+
+
+
+userSchema.pre("save", async function(next){
+
+    if(!this.isModified("password")) return next();
+    this.password= bcrypt.hash(this.password,10)
+    next()
+
+})
+
+userSchema.methods.isPasswordCoreect=async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+
+
+userSchema.methods.generateAccessToken= function(){
+
+   return jwt.sign(
+    {
+        _id:this.id,
+        email:this.email,
+        username:this.username,
+        fullname:this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+    )
+}
+
+
+
+userSchema.methods.generateRefreshToken= function(){
+    return jwt.sign(
+        {
+            _id:this.id,
+            email:this.email,
+            username:this.username,
+            fullname:this.fullname
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+        )
+    }
+
+
+
+
+
+
+
+
+
+
 
 export const User=mongoose.model("User",userSchema)
